8785d965e1a4baaa00d3f7697e984f20794a6352
mariyatarasenko88 <tmv1231@yandex.ru>
2023-03-07 21:00:22 +0300

    Initial commit

---
+++ src/ServiceStation.java
@@ -0,0 +1,21 @@
+public class ServiceStation {
+    if (car != null) {
+        System.out.println("Обслуживаем " + car.modelName);
+        for (int i = 0; i < car.wheelsCount; i++) {
+            car.updateTyre();
+        }
+        car.checkEngine();
+    } else if (truck != null) {
+        System.out.println("Обслуживаем " + truck.modelName);
+        for (int i = 0; i < truck.wheelsCount; i++) {
+            truck.updateTyre();
+        }
+        truck.checkEngine();
+        truck.checkTrailer();
+    } else if (bicycle != null) {
+        System.out.println("Обслуживаем " + bicycle.modelName);
+        for (int i = 0; i < bicycle.wheelsCount; i++) {
+            bicycle.updateTyre();
+        }
+    }
+}
