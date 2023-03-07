7598f5344117cb8a2df67e065c774cc047264522
mariyatarasenko88 <tmv1231@yandex.ru>
2023-03-07 23:08:53 +0300

    Homework2.2 is donne

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,12 @@
-public class Bicycle {
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+public class Bicycle extends Transport {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
-
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+    }
 }
--- src/Truck.java
+++ src/Truck.java
@@ -1,16 +1,18 @@
-public class Truck {
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+public class Truck extends Transport implements Check {
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
-
+    @Override
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
     }
-}
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+            checkEngine();
+            checkTrailer();
+        }
+    }
